4869727a9c3f3577025c247403c741e9428424bc
Abbie Gonzalez <abbie@abbiecod.es>
2011-12-07 22:28:08 -0500

    changes

--- receiver.php
+++ receiver.php
@@ -1,18 +1,17 @@
 <?php
 
+include 'emc_config.php';
 /* Grab Data from Smart Phone and place into CSV to be parsed by Yahoo! */
-
    $coordinatepair =  $_REQUEST['coordinate'];
-   if (!($CallList = fopen('./CallList.csv', 'a')))
-    return;
-    $coordinatepair = "\"" . date("r") . "\"," . $coordinatepair . "\n";
-    fprintf($CallList, $coordinatepair);
-    fclose($CallList);
+   //if (!($CallList = fopen('./CallList.csv', 'a')))
+   // return;
+   // $coordinatepair = "\"" . date("r") . "\"," . $coordinatepair . "\n";
+   // fprintf($CallList, $coordinatepair);
+   // fclose($CallList);
     echo "done";
-    $to = "abbie.gonzalez@gmail.com, t68v3z-yx5964@twittermail.com, joe140scamp@m.facebook.com";
 
-    $subject = $SUBJECT . "Call me: " . $PHONE . $coordinatepair;
-    $body = "";
+    $subject = $SUBJECT . " Call me: " . $PHONE . $coordinatepair;
+    $body = $subject;
     $header = "From:".$FROM;
     mail($SENDTO, $subject, $body, $header);
 
