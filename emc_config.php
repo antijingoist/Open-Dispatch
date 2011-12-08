2ee1283273dcf7e233334b55af379279c3dca2e0
Abbie Gonzalez <abbie@abbiecod.es>
2011-12-08 12:17:10 -0500

    Update emc_config.php

--- emc_config.php
+++ emc_config.php
@@ -3,20 +3,25 @@
 
 // Site Plugin Settings
 // Banner: 1= Display banner, 2=redirect, 0= none
-	$BANNER=0;
-    $REDIRECT_PAGE='';
-    $BANNER_PAGE='';
+   $BANNER=0;
+   $REDIRECT_PAGE='';
+   $BANNER_PAGE='';
     
 //Server settings
-	$NODEMODE=0;  // 
-	$TOKEN_FILE='tok.en';
-    $PING='';
-    
+
+   // Should this act as a fowarding/display node?
+   $NODEMODE=0;
+   // Token notifies that you've pulled your trigger
+   $TOKEN_FILE='tok.en';
+   
+   // Servers to ping with your status
+   $PING='';
+   
 
 	//email settings
-	$SENDTO='test@abbiecod.es'; //email addresses to send notifies to, comma separated. 
-	$FROM='test@abbiecod.es';
-
-	$SUBJECT="Test of emergency broadcast system";
-	$PHONE='555-555-5555';
+   $SENDTO  ='test@abbiecod.es'; //email addresses to send notifies to, comma separated. 
+   $FROM    ='test@abbiecod.es';
+   $SUBJECT ="Test of emergency broadcast system";
+   $BODY    = $SUBJECT;
+   $PHONE   ='555-555-5555';
 ?>
