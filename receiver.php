b90b9c4df8293ae0729edc567f2ecc676b7c2b72
Abbie Gonzalez <abbie@abbiecod.es>
2011-12-19 00:15:06 +0000

    added message override by passing messg=

--- receiver.php
+++ receiver.php
@@ -1,9 +1,16 @@
 <?php
    include 'emc_config.php';
 /* Grab Data from Smart Phone and place into CSV */
-   $location =  _INPUT('loc');
-   $ping = _INPUT('ping');
+   $location    = _INPUT('loc');
+   $ping        = _INPUT('ping');
+   $msgOverride = _INPUT('messg');
    
+   if (isset($msgOverride)){
+      $message = $msgOverride;
+   }else{
+      $message = $MSG_BODY;
+   }
+
    if ($TRACKING == 1){
       if (!($data = fopen('./tracking.csv', 'a')))
          return;
@@ -11,21 +18,15 @@
       fprintf($data, $dataline);
       fclose($data);
    }
-   
+
    if ($ping==1){
       $location_map = str_replace("%location%", $location, $MAP_API);
       $subject = $SUBJECT . " " . $PHONE . ' ' . $location_map;
-      $body = str_replace('%location%', $location, $MSG_BODY);
+      $body = str_replace('%location%', $location, $message);
       $body = str_replace('%map%',$location_map,$body);
       $header = "From:".$FROM;
       mail($SENDTO, $subject, $body, $header);
    }
    echo "done";
 
-/*
-   if (!($CallList = fopen('./CallList.csv', 'w')))
-    return;
-    fprintf($CallList, "");
-   fclose($CallList);
-*/
 ?>
